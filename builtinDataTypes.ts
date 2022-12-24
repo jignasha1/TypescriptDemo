84e3b8ed651aa75e5e5f28102db8d86e5afc6ec9
jignasha1 <jiga.er@gmail.com>
2022-12-24 23:24:22 +0530

    American guy commit

--- builtinDataTypes.ts
+++ builtinDataTypes.ts
@@ -25,6 +25,9 @@ function hello():void
     console.log("This is welcome message")
     console.log("Test1");
     console.log("Test2");
+    console.log("Test3");
+    console.log("Test4");
+    console.log("Test5");
 }
 
 //Null
