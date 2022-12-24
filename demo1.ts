382b74255a8bb0026086e5c046502fd83cf5bdd6
jignasha1 <jiga.er@gmail.com>
2022-12-25 00:23:54 +0530

    add some more text

--- demo1.ts
+++ demo1.ts
@@ -5,3 +5,11 @@ console.log("c");
 console.log("d");
 console.log("e");
 console.log("f");
+
+console.log("Numbers");
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+console.log("6")
